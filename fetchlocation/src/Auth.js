22549521510647ad924f8934e5895f67a6de6d04
kashyapanjali <anjalikashyap9608@gmail.com>
2024-09-18 08:24:38 +0530

    frontendInitialization

---
+++ fetchlocation/src/Auth.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Auth.css";
+export default function Auth() {
+  return (
+    <div className="auth">
+      <h1>SignUp</h1>
+      <form></form>
+    </div>
+  );
+}
