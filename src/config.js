7f32fb96d0b29c9a09160dde5052229dfdaf0308
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-05-04 12:27:02 +0530

    add aws url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Central configuration file for the application
 
 const config = {
-  API_BASE_URL: "http://localhost:5000/api",
-  WS_URL: "ws://localhost:5000",
+  API_BASE_URL: "http://13.203.227.147:3000/api",
+  WS_URL: "ws://13.203.227.147:3000",
   DEBUG: true  // Enable debug mode to see more detailed error messages
 };
 
