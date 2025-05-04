afbb4a0f3b026d67562dd8ad03fa9e86e78dc07e
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-05-04 15:17:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 // Central configuration file for the application
 
 const config = {
+<<<<<<< HEAD
 	API_BASE_URL: "http://localhost:5000/api",
 	WS_URL: "ws://localhost:5000",
 	DEBUG: true, // Enable debug mode to see more detailed error messages
+=======
+  API_BASE_URL: "http://13.203.227.147:3000/api",
+  WS_URL: "wss://13.203.227.147"
+>>>>>>> parent of 6f3749e (check in local)
 };
 
 export default config;
