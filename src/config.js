872e90cbb8306f3a8b31ae2b0fe1cb4bb61513e2
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-05-04 15:04:31 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,12 @@
 const config = {
   API_BASE_URL: "http://13.203.227.147:3000/api",
 <<<<<<< HEAD
+<<<<<<< HEAD
   WS_URL: "ws://13.203.227.147:3000",
   DEBUG: true  // Enable debug mode to see more detailed error messages
+=======
+  WS_URL: "wss://13.203.227.147"
+>>>>>>> parent of 6f3749e (check in local)
 =======
   WS_URL: "wss://13.203.227.147"
 >>>>>>> parent of 6f3749e (check in local)
